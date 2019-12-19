a1df4cb9576ebc084101841f7db4253ee5e18797
joaogqueiroz <joaogilberto350@hotmail.com>
2019-12-18 22:00:08 -0300

    update

---
+++ meu_javascript.js
@@ -0,0 +1,11 @@
+// comenta apenas uma linha
+
+/*
+
+comenta 
+multiplas 
+linhas
+*/
+
+
+alert('Teste de inclusão de javascript') 
---
+++ variaveis.js
@@ -0,0 +1,21 @@
+//string
+var texto = 'Aula de JavaScript';
+
+
+//float
+var numFracionado = 10.7;
+
+//int
+var numero = -7;
+
+//boolean
+var teste = true; //true -> verdadeiro ou false -> falso
+
+//formas de exibir conteudo
+ alert (texto);//pop-up
+
+ document.write(numero);//imprime o conteudo no corpo da pagina
+
+ console.log(numFracionado);//imprime o conteudo no log do navegador
+
+ 
