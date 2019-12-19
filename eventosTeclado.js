52cfa75d8964002cc6ee03a9b84051589e9e4c1e
joaogqueiroz <joaogilberto350@hotmail.com>
2019-12-19 01:06:46 -0300

    upgrade

---
+++ eventosTeclado.js
@@ -0,0 +1,24 @@
+function msgTeclado1(){
+	//onclick
+	//ondblclick
+	//onmousedown
+	//onmouseover
+	//onmouseout
+	alert('Seja bem vindo(a), você pressionou alguma tecla')	
+}
+function msgTeclado2(){
+	//onclick
+	//ondblclick
+	//onmousedown
+	//onmouseover
+	//onmouseout
+	alert('Seja bem vindo(a), você pressionou alguma tecla')
+}
+function msgTeclado3(){
+	//onclick
+	//ondblclick
+	//onmousedown
+	//onmouseover
+	//onmouseout
+	alert('Seja bem vindo(a), você soltou alguma tecla')
+}
