ece2847ed72c63d3b277a251129de6b7dcc41d47
joaogqueiroz <joaogilberto350@hotmail.com>
2020-01-26 22:16:03 -0300

    Calculadora

--- calculadora.js
+++ calculadora.js
@@ -1,41 +1,48 @@
 
 function calcular(){
-	var operacao = document.getElementById("operacao").value;
+	var operacao = document.getElementById('operacao').value;
 
-	var num1 = document.getElementById("num1").value;
+	var num1 = document.getElementById('num1').value;
 
-	var num2 = document.getElementById("num2").value;
+	var num2 = document.getElementById('num2').value;
 
-	if(num1 == "" || num1 == null){
-		alert("favor digitar um numero valido");
+	//forçando o tipo de variável, pois recuperamos campos em string e não numericos
+	num1 = parseFloat(num1);
+
+	num2 = parseFloat(num2);
+
+
+	if(num1 == '' || num1 == null){
+		alert('Favor digitar um número valido para operação');
 		return false;
 	}
-
-	if(num2 == "" || num2 == null){
-		alert("favor digitar um numero valido");
+	if(num2 == '' || num2 == null){
+		alert('Favor digitar um número valido para operação');
 		return false;
 	}
-
+	var resultado = null;
 	switch(operacao){
+		case '1': //subtração
+			resultado = num1 - num2;
+			break;
 
-		case "1" //subtração
+		case '2': //adição
+			resultado = num1 + num2;
 			break;
-			
-		case "2" //adicação
-			break;
-			
-		case "3" //multiplicação
-			break;
-			
-		case "4" //divisão
-			break;
-		default
-			alert("Operacao invalida");
-			return false;
 
+		case '3': // multiplicação
+			resultado = num1 * num2;
+			break;
+
+		case '4': //divisão
+			resultado = num1 / num2;
+			break;
+
+		default:
+		alert('Opção selecionada invalida');
+		return false;
 	}
 
-
-	alert(operacao);
+	document.getElementById('resultado').value = resultado;
 
 }
